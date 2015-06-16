5a54a9461c0318240eff3d197592ce93f997fec7
Steve P <steevstevep@gmail.com>
2015-06-15 23:57:30 -0700

    Initial commit

---
+++ start.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2 
+import os, sys
+from ctypes import CDLL
+
+os.environ['SteamAppId'] = '22320'
+
+# Steam Overlay
+os.environ['LD_PRELOAD'] = os.getenv('HOME') + '/.local/share/Steam/ubuntu12_64/gameoverlayrenderer.so'
+
+steam_api = CDLL('./libsteam_api.so')
+try:
+    steam_api.SteamAPI_Init()
+except:
+    print 'Failed to initialize Steam API'
+    sys.exit(1)
+
+os.system('openmw-launcher')
